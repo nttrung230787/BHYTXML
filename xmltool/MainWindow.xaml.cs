68ceedf896be68acabaa7e66eeebe3dfbf5b2f38
unknown <nttrung230787@gmail.com>
2023-03-08 11:29:39 +0700

    Them su kien chao

--- xmltool/MainWindow.xaml.cs
+++ xmltool/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace xmltool
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Xin chao Trung");
+        }
     }
 }
